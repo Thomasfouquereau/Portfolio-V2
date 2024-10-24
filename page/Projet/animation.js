fb199b1bca2f409ae29bc8c1e71677d9a83f0faa
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-10-24 08:37:51 +0200

    Add page for "CarteVL" project fix mobile mode for all projet

--- page/Projet/animation.js
+++ page/Projet/animation.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carreLine1 = document.querySelector('.carre-line_1');
-    const enfants1 = Array.from(carreLine1.children);
-    gsap.set(enfants1, { opacity: 0 });
-    gsap.to(enfants1, { opacity: 1, duration: 1, stagger: 0.1, delay: 0.5 });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     const carreLine1 = document.querySelector('.carre-line_1');
+//     const enfants1 = Array.from(carreLine1.children);
+//     gsap.set(enfants1, { opacity: 0 });
+//     gsap.to(enfants1, { opacity: 1, duration: 1, stagger: 0.1, delay: 0.5 });
+// });
